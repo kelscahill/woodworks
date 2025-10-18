9e0317d1bec0a0d8b9ae00b98c6fd84fb4a4179c
Kelsey Cahill <kelscahill@gmail.com>
2025-10-18 11:15:54 -0400

    Editor styles

--- wp-content/themes/woodworks/app/setup.php
+++ wp-content/themes/woodworks/app/setup.php
@@ -23,7 +23,7 @@ add_action('wp_enqueue_scripts', function () {
  * @return void
  */
 add_action('enqueue_block_editor_assets', function () {
-    bundle('editor')->enqueue();
+  bundle('editor')->enqueue();
 }, 100);
 
 /**
