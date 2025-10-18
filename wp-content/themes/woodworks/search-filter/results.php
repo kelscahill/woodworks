442091f3bc48ecd3511e3c4fc72f726e5ccfede3
Kelsey Cahill <kelscahill@gmail.com>
2025-10-18 09:23:45 -0400

    Fix for search and filter

--- wp-content/themes/woodworks/search-filter/results.php
+++ wp-content/themes/woodworks/search-filter/results.php
@@ -1,10 +1,25 @@
 <?php
   global $search_filter_query;
-  $searchandfilterId = $query->query['search_filter_query_id'];
-  $sf_current_query = \Search_Filter\Queries\Query::find( array(
-    'id' => $searchandfilterId
-  ) );
-  $pagination_type = $sf_current_query->get_attributes()['resultsPaginationType'];
+  $pagination_type = 'default'; // Default pagination type
+  $searchandfilterId = null;
+
+  // Check if $search_filter_query exists and has the expected structure
+  if ( isset( $search_filter_query ) &&
+       isset( $search_filter_query->query ) &&
+       isset( $search_filter_query->query->query_vars['search_filter_id'] ) ) {
+
+    $searchandfilterId = $search_filter_query->query->query_vars['search_filter_id'];
+    $sf_current_query = \Search_Filter\Queries\Query::find( array(
+      'id' => $searchandfilterId
+    ) );
+
+    if ( ! is_wp_error( $sf_current_query ) ) {
+      $attributes = $sf_current_query->get_attributes();
+      if ( isset( $attributes['resultsPaginationType'] ) ) {
+        $pagination_type = $attributes['resultsPaginationType'];
+      }
+    }
+  }
 ?>
 <div id="search-filter-<?php echo $searchandfilterId; ?>" class="c-search-filter u-spacing--double">
   <?php if ($query->have_posts()) : ?>
